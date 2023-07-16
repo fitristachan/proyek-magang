eec83ac9ba9527e18da62a8864bf3e383189d0d6
fitristachan <120694803+fitristachan@users.noreply.github.com>
2023-07-17 02:18:34 +0700

    ketinggalan

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -83,7 +83,7 @@
       <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8 text-red-600" viewBox="0 0 20 20" fill="currentColor">
         <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
     ` </svg>
-        <p class="ml-3 text-sm font-bold text-red-600"><?php echo $session->getFlashdata('error'); ?></p>
+        <p class="ml-3 text-sm font-bold text-red"><?php echo $session->getFlashdata('error'); ?></p>
     </div>
     <span @click="nofifiction=false;" class="inline-flex items-center cursor-pointer ml-4">
       <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 text-red-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
