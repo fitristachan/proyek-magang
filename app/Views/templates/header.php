61b5afbc6cfd10d7ffa1efeb5bdbae08f128a640
fitristachan <120694803+fitristachan@users.noreply.github.com>
2023-07-17 08:29:11 +0700

    ketinggalan 2

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -72,7 +72,7 @@
               </button>
             </div>
             <nav class="flex flex-col flex-1 w-64 p-4 mt-4">
-              <a href="#" class="flex items-center space-x-2">
+              <a href="/home/index" class="flex items-center space-x-4">
                 <svg
                   class="w-6 h-6"
                   aria-hidden="true"
@@ -90,9 +90,29 @@
                 </svg>
                 <span>Home</span>
               </a>
+              <?php if($session->role_id == 1){ ?>
+              <a href="/auth/viewUser" class="flex items-center space-x-4 mt-4">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+                </svg>
+                <span>Users</span>
+                <?php }?>
+              </a>
+              <a href="#" class="flex items-center space-x-4 mt-4">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+              </svg>
+                <span>Decision</span>
+              </a>
+              <a href="#" class="flex items-center space-x-4 mt-4">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
+              </svg>
+                <span>History</span>
+              </a>
             </nav>
             <div class="flex-shrink-0 p-4">
-              <button class="flex items-center space-x-2">
+              <a href="/auth/logout" class="flex items-center space-x-4">
                 <svg
                   aria-hidden="true"
                   class="w-6 h-6"
@@ -109,7 +129,7 @@
                   />
                 </svg>
                 <span>Logout</span>
-              </button>
+              </a>
             </div>
           </div>
         </div> 
